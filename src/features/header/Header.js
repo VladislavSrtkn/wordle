92ccc5141733edbe805505695e957e2040b8cca0
VladislavSrtkn <vladislavsrtkn@gmail.com>
2023-04-23 16:07:35 +0200

    header design changed

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -17,7 +17,7 @@ export default function Header({
   const iconClasses = 'bi px-1 fs-4 me-2 header-icon pb-2';
 
   return (
-    <Row className='justify-content-between border-bottom py-2 mb-3'>
+    <Row className='justify-content-between py-2 mb-3 header'>
       <Col xs='auto'>
         <CustomTooltip text={tooltipRules}>
           <i className={`bi-question-circle ${iconClasses}`} onClick={onShowRules} />
