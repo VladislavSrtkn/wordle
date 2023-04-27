018a992395984c42559168bd2ca877551fdd3bd8
VladislavSrtkn <vladislavsrtkn@gmail.com>
2023-04-27 09:29:01 +0200

    header width changed

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -10,25 +10,27 @@ export default function Header({
   const iconClasses = 'bi px-1 fs-4 me-2 header-icon pb-2';
 
   return (
-    <Row className='justify-content-between py-2 mb-3 header'>
-      <Col xs='auto'>
-        <i className={`bi-question-circle-fill ${iconClasses}`} onClick={onShowRules} />
+    <Row className='py-2 mb-3 header justify-content-center'>
+      <Col className='d-flex justify-content-between'>
+        <Col xs='auto'>
+          <i className={`bi-question-circle-fill ${iconClasses}`} onClick={onShowRules} />
 
-        {theme === 'light' ? (
-          <i className={` bi-moon-fill ${iconClasses}`} onClick={onChangeTheme} />
-        ) : (
-          <i className={`bi-sun-fill ${iconClasses}`} onClick={onChangeTheme} />
-        )}
-      </Col>
+          {theme === 'light' ? (
+            <i className={` bi-moon-fill ${iconClasses}`} onClick={onChangeTheme} />
+          ) : (
+            <i className={`bi-sun-fill ${iconClasses}`} onClick={onChangeTheme} />
+          )}
+        </Col>
 
-      <Col xs='auto' as='h1' className='fs-2'>
-        Wordle
-      </Col>
+        <Col xs='auto' as='h1' className='fs-2'>
+          Wordle
+        </Col>
 
-      <Col xs='auto'>
-        <i className={`bi-globe ${iconClasses}`} onClick={onShowLanguageBanner} />
+        <Col xs='auto'>
+          <i className={`bi-globe ${iconClasses}`} onClick={onShowLanguageBanner} />
 
-        <i className={`bi-reception-4 ${iconClasses}`} onClick={onShowStatistics} />
+          <i className={`bi-reception-4 ${iconClasses}`} onClick={onShowStatistics} />
+        </Col>
       </Col>
     </Row>
   );
